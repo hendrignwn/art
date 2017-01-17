3a4141116083bc0a519906459e8202c25a122177
Hendri <hendri.gnw@gmail.com>
2017-01-17 15:51:00 +0700

    add class Application

---
+++ components/Application.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\components;
+
+use app\models\Config;
+
+/**
+ * Description of Application
+ *
+ * @author Hendri
+ */
+class Application extends \yii\web\Application
+{
+	public function init() {
+		parent::init();
+		
+		//$this->name = Config::getAppName();
+		
+		return true;
+	}
+}
