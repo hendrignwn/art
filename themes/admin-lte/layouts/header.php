42c4e5bd3b4e99b949c2308b81912abd5aaf340b
Hendri <hendri.gnw@gmail.com>
2017-01-17 17:05:24 +0700

    add theme admin-lte for template backend system

---
+++ themes/admin-lte/layouts/header.php
@@ -0,0 +1,59 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+
+<header class="main-header">
+
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+
+                <?php if (!Yii::$app->user->isGuest) { ?>
+				<li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<i class="fa fa-user-o"></i>
+                        <span class="hidden-xs"><?= Yii::$app->user->identity->username ?></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+							<i class="fa fa-user-o fa-5x" style="color:#fff;"></i>
+
+                            <p>
+                                <?= Yii::$app->user->identity->detail_lengkap ?>
+								<small><?= Yii::t('app', 'Join since') ?>: <?= FormatConverter::indoDateFormat(Yii::$app->user->identity->join_at, '%B, %Y') ?></small>
+                                <small><?= Yii::t('app', 'Last Login') ?>: <?= FormatConverter::dateFormat(Yii::$app->user->identity->last_login, 'd M Y H:i:s') ?></small>
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                               <?= Html::a(
+									Yii::t('app.menu', 'Sign out'),
+									['default/logout'],
+									['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
+								) ?>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+				<?php } ?>
+                
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ themes/admin-lte/layouts/left.php
@@ -0,0 +1,35 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <?= dmstr\widgets\Menu::widget(
+            [
+                'options' => ['class' => 'sidebar-menu'],
+                'items' => [
+                    ['label' => Yii::t('app.menu', 'Main Menu'), 'options' => ['class' => 'header']],
+                    ['label' => Yii::t('app.menu', 'Dashboard'), 'icon' => 'fa fa-dashboard', 'url' => ['default/index']],
+                    ['label' => Yii::t('app.menu', 'Sign in'), 'icon' => 'fa fa-sign-in', 'url' => ['default/login'], 'visible' => Yii::$app->user->isGuest],
+                    [
+                        'label' => Yii::t('app.menu', 'Master'),
+                        'icon' => 'fa fa-folder-o',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => Yii::t('app.menu', 'Banner'), 'icon' => 'fa fa-eercast', 'url' => ['banner/index']],
+                        ],
+                    ],
+                    [
+                        'label' => Yii::t('app.menu', 'Blog'),
+                        'icon' => 'fa fa-folder-o',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => Yii::t('app.menu', 'Post'), 'icon' => 'fa fa-plus-square-o', 'url' => ['/gii']],
+                            ['label' => Yii::t('app.menu', 'Post Categories'), 'icon' => 'fa fa-reorder', 'url' => ['/gii']],
+                        ],
+                    ],
+                ],
+            ]
+        ) ?>
+
+    </section>
+
+</aside>
