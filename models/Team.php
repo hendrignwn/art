94f4cf2b32edda94e3a7ad6deb673a62a441ee84
Hendri <hendri.gnw@gmail.com>
2017-01-20 13:22:08 +0700

    update Team that is can be upload file

--- models/Team.php
+++ models/Team.php
@@ -2,7 +2,11 @@
 
 namespace app\models;
 
+use app\helpers\Url;
 use Yii;
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\web\UploadedFile;
 
 /**
  * This is the model class for table "team".
@@ -19,9 +23,30 @@ use Yii;
  * @property string $updated_at
  * @property integer $created_by
  * @property integer $updated_by
+ * 
+ * @property string $path
  */
-class Team extends \app\models\BaseActiveRecord
+class Team extends BaseActiveRecord
 {
+    /**
+     * @var UploadedFile
+     */
+    public $photoFile;
+    
+    private $_path;
+    
+    public function init()
+    {
+        parent::init();
+        
+        $this->path = 'web/uploads/team/';
+        if (!is_dir(Yii::getAlias('@app/' . $this->path))) {
+            mkdir(Yii::getAlias('@app/' . $this->path));
+        }
+
+        return true;
+    }
+    
     /**
      * @inheritdoc
      */
@@ -36,15 +61,37 @@ class Team extends \app\models\BaseActiveRecord
     public function rules()
     {
         return [
-            [['first_name', 'last_name', 'professional', 'photo', 'description'], 'required'],
+            [['first_name', 'last_name', 'professional', 'description'], 'required'],
             [['description'], 'string'],
             [['status', 'created_by', 'updated_by'], 'integer'],
-            [['created_at', 'updated_at'], 'safe'],
+            [['photo', 'created_at', 'updated_at'], 'safe'],
             [['first_name'], 'string', 'max' => 30],
             [['last_name'], 'string', 'max' => 50],
             [['professional'], 'string', 'max' => 100],
             [['photo', 'social_account'], 'string', 'max' => 255],
+            [['status'], 'default', 'value' => self::STATUS_ACTIVE],
+            [['photoFile'], 'file', 'skipOnEmpty' => true, 'checkExtensionByMimeType' => false,
+                'extensions' => ['jpg', 'jpeg', 'png'],
+                'maxSize' => 1024 * 1024 * 1],
         ];
+    }
+    
+    /**
+     * - delete photoFile
+     * 
+     * @return type
+     */
+    public function beforeDelete()
+    {
+        /* todo: delete the corresponding file in storage */
+        $this->deleteFile();
+        
+        return parent::beforeDelete();
+    }
+    
+    protected function deleteFile()
+    {
+        @unlink(Yii::getAlias('@app/' . $this->path) . $this->photo);
     }
 
     /**
@@ -66,5 +113,90 @@ class Team extends \app\models\BaseActiveRecord
             'created_by' => Yii::t('app', 'Created By'),
             'updated_by' => Yii::t('app', 'Updated By'),
         ];
+    }
+    
+    /**
+     * - process upload file
+     * 
+     * @param type $insert
+     * @return type
+     */
+    public function beforeSave($insert) 
+    {
+        $this->processUploadFile();
+        
+        return parent::beforeSave($insert);
+    }
+    
+    /**
+     * process uploaded file
+     * 
+     * @return boolean
+     */
+    public function processUploadFile()
+    {
+        if (!empty($this->photoFile)) {
+            $this->deleteFile();
+
+            $path = str_replace('web/', '', $this->path);
+            
+            // generate filename
+            $filename = Inflector::slug($this->first_name . '-' . Yii::$app->security->generateRandomString(20));
+            $filename = $filename . '.' . $this->photoFile->extension;
+            
+            $this->photoFile->saveAs($path . $filename);
+            $this->photo = $filename;
+        }
+
+        return true;
+    }
+    
+    /**
+     * get url file
+     * 
+     * @return type
+     */
+    public function getPhotoUrl() 
+    {
+        if (empty($this->photo)) {
+            return null;
+        }
+
+        $path = $this->path . $this->photo;
+
+        if (!file_exists(Yii::getAlias('@app/' . $path))) {
+            return null;
+        }
+
+        return Url::to('@' . $path, true);
+    }
+
+    public function getPhotoUrlHtml($name = null, $options = ['target' => '_blank']) 
+    {
+        $name = $name ? $name : $this->first_name;
+
+        if (!$this->getPhotoUrl()) {
+            return $name;
+        }
+
+        return Html::a($name, $this->getPhotoUrl(), $options);
+    }
+
+    /**
+     * set path
+     * 
+     * @param type $value
+     */
+    public function setPath($value)
+    {
+        $this->_path = $value;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->_path;
     }
 }
