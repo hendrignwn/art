d5959440978b0084b568b9a90441ae3910fa44c6
Hendri <hendri.gnw@gmail.com>
2017-01-23 18:28:58 +0700

    update Menu model

--- models/Menu.php
+++ models/Menu.php
@@ -19,9 +19,17 @@ use Yii;
  * @property string $updated_at
  * @property integer $created_by
  * @property integer $updated_by
+ * 
+ * @property Menu[] $parent
+ * @property Menu[] $parents
  */
 class Menu extends \app\models\BaseActiveRecord
 {
+    const CATEGORY_MAIN = 1;
+    const CATEGORY_MAIN_FOOTER = 2;
+    const CATEGORY_BACKEND = 10;
+    
+    
     /**
      * @inheritdoc
      */
@@ -36,7 +44,7 @@ class Menu extends \app\models\BaseActiveRecord
     public function rules()
     {
         return [
-            [['parent_id', 'is_absolute_url', 'status', 'order', 'created_by', 'updated_by'], 'integer'],
+            [['parent_id', 'is_absolute_url', 'status', 'order', 'created_by', 'updated_by', 'category'], 'integer'],
             [['name', 'url', 'order'], 'required'],
             [['option'], 'string'],
             [['created_at', 'updated_at'], 'safe'],
@@ -57,6 +65,7 @@ class Menu extends \app\models\BaseActiveRecord
             'name' => Yii::t('app', 'Name'),
             'url' => Yii::t('app', 'Url'),
             'is_absolute_url' => Yii::t('app', 'Is Absolute Url'),
+            'category' => Yii::t('app', 'Category'),
             'option' => Yii::t('app', 'Option'),
             'status' => Yii::t('app', 'Status'),
             'order' => Yii::t('app', 'Order'),
@@ -65,5 +74,95 @@ class Menu extends \app\models\BaseActiveRecord
             'created_by' => Yii::t('app', 'Created By'),
             'updated_by' => Yii::t('app', 'Updated By'),
         ];
+    }
+    
+    /**
+     * get menu parent
+     * 
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParent()
+    {
+        return $this->hasOne(self::className(), ['id' => 'parent_id']);
+    }
+    
+    /**
+     * get menu children
+     * 
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParents()
+    {
+        return $this->hasMany(self::className(), ['parent_id' => 'id']);
+    }
+    
+    /**
+     * get menus
+     * 
+     * @param type $category
+     * @param type $options
+     * @param type $childOptions
+     * @return type
+     */
+    public function getMenus($category = self::CATEGORY_MAIN, $options = [], $childOptions = [])
+    {
+        $query = self::find()
+                ->where([
+                    'category' => $category,
+                    'parent_id' => null,
+                ])
+                ->actived()
+                ->all();
+        
+        $items = [];
+        foreach ($query as $data) {
+            $items[$data->id]['label'] = $data->name;
+            $items[$data->id]['id'] = $data->id;
+            $items[$data->id]['url'] = $data->url;
+            $items[$data->id]['options'] = $options;
+            
+            if ($data->parents != null) {
+                //var_dump($data->parents);continue;
+                $items[$data->id]['url'] = '#';
+                $items[$data->id]['options'] = $childOptions;
+                $items[$data->id]['items'] = $this->getChildren($items[$data->id]['items'], $data->parents);
+            }
+        }
+        
+        return $items;
+    }
+    
+    /**
+     * get children
+     * 
+     * @param type $items
+     * @param type $children
+     * @return type
+     */
+    protected function getMenuChildren(&$items, $parents, $options = [], $childOptions = [])
+    {
+        foreach ($parents as $data) {
+            $items[$data->id]['label'] = $data->name;
+            $items[$data->id]['id'] = $data->id;
+            $items[$data->id]['url'] = $data->url;
+            $items[$data->id]['options'] = $options;
+            if ($data->parents != null) {
+                $items[$data->id]['url'] = '#';
+                $items[$data->id]['options'] = $childOptions;
+                $items[$data->id]['items'] = $this->getMenuChildren($items[$data->id]['items'], $data->parents, $options, $childOptions);
+            }
+        }
+        
+        return $items;
+    }
+    
+    /**
+     * get option
+     * 
+     * @return eval
+     */
+    public function getOption()
+    {
+        return eval($this->option);
     }
 }
