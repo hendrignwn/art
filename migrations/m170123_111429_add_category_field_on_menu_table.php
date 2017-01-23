13e50542ce446872dfb743e83764691c5b9ad323
Hendri <hendri.gnw@gmail.com>
2017-01-23 18:28:37 +0700

    add new category field in the menu

---
+++ migrations/m170123_111429_add_category_field_on_menu_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170123_111429_add_category_field_on_menu_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('menu', 'category', $this->smallInteger()->notNull()->defaultValue('1')->after('option'));
+    }
+
+    public function safeDown()
+    {
+        $this->dropColumn('menu', 'category');
+    }
+}
