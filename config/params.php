7ebfb3167a3dcbd3fd7fe1aea7b75fa419aff160
Hendri <hendri.gnw@gmail.com>
2017-01-24 19:01:06 +0700

    add some parameters

--- config/params.php
+++ config/params.php
@@ -2,4 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'mainUrl' => 'http://www.atc.co.id',
+    'mobileUrl' => 'http://m.atc.co.id',
+    'jsBlock' => 0,
+    'modalBlock' => 0,
 ];
