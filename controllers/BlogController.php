97233e33afff1a9dd334be9db0a8ea0f8dc1331a
Hendri <hendri.gnw@gmail.com>
2017-01-31 10:09:20 +0700

    update blog controller

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -2,24 +2,54 @@
 
 namespace app\controllers;
 
+/**
+ * BlogController
+ * 
+ * @author Hendri <hendri.gnw@gmail.com>
+ */
 class BlogController extends \app\controllers\BaseController
 {
+    /**
+     * show listing blog
+     * 
+     * @return string
+     */
     public function actionIndex()
     {
         return $this->render('index');
     }
     
+    /**
+     * show blog detail
+     * 
+     * @param type $year d+ {4}
+     * @param type $month d+ {2}
+     * @param type $slug w+
+     * @return type
+     */
     public function actionDetail($year, $month, $slug)
     {
         return $this->render('detail', []);
     }
     
+    /**
+     * show listing blog by category
+     * 
+     * @param type $slug w+
+     * @return type
+     */
     public function actionCategory($slug)
     {
         return $this->render('category', []);
     }
     
-    public function actionTag($slug)
+    /**
+     * show listing blog by tag
+     * 
+     * @param type $name w+
+     * @return type
+     */
+    public function actionTag($name)
     {
         return $this->render('tag', []);
     }
