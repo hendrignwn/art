7b29c5bf3bd25c21997b5862b841c7ce9a9db6fd
Hendri <hendri.gnw@gmail.com>
2017-02-02 18:25:17 +0700

    add new column

---
+++ migrations/m170202_111643_add_column_technology_on_portfolio_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170202_111643_add_column_technology_on_portfolio_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('portfolio', 'technology', $this->string(100)->notNull()->after('completed_on'));
+    }
+
+    public function safeDown()
+    {
+        $this->dropColumn('portfolio', 'technology');
+    }
+}
