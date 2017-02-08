3785ba7878abd75cbe853a244817648bed1f738f
Hendri <hendri.gnw@gmail.com>
2017-02-08 22:51:02 +0700

    update content new contact

--- mail/contact/new-contact-to-admin.php
+++ mail/contact/new-contact-to-admin.php
@@ -5,4 +5,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* @var $model app\models\Contact */
 
+?>
+
+Hello Admin,
+<br/>
+Someone calling you via Contact Form with details in following below:
+<br/>
+<table>
+    <tr>
+        <td>Name</td>
+        <td>:</td>
+        <td><?= $model->getFullName() ?></td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>:</td>
+        <td><?= $model->email ?></td>
+    </tr>
+    <tr>
+        <td>Phone</td>
+        <td>:</td>
+        <td><?= $model->phone ?></td>
+    </tr>
+    <tr>
+        <td>Subject</td>
+        <td>:</td>
+        <td><?= $model->subject ?></td>
+    </tr>
+</table>
+In here is description: <br/>
+<?= $model->description ?>
+
+<br/><br/>
+Thanks
