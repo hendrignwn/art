98bf859e6af31bd663f43e3363b1b2ae3ef19fc1
Hendri <hendri.gnw@gmail.com>
2017-02-22 19:31:14 +0700

    add google analytics

--- themes/v1/views/layouts/main.php
+++ themes/v1/views/layouts/main.php
@@ -1,14 +1,21 @@
 <?php
 
-/* @var $this View */
-/* @var $content string */
-
 use app\assets\AppAsset;
 use yii\helpers\Html;
 use yii\web\View;
 use yii\widgets\Breadcrumbs;
 
+/* @var $this View */
+/* @var $content string */
+
 AppAsset::register($this);
+$this->registerJs("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92397862-1', 'auto');
+  ga('send', 'pageview');", View::POS_END, 'google-analytic');
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
